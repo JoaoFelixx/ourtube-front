904506fbe680704c6ed0bae398ecdaa6454c888d
Felix developer <felixxdeveloper@gmail.com>
2022-08-11 19:59:23 -0300

    REF - Better the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,14 @@
 import Routes from 'Router';
-import { IconContext } from "react-icons";
-import { AuthProvider } from 'Context/AuthProvider';
-import { ToastContainer } from 'react-toastify';
-import { VideosProvider } from 'Context/VideosProvider';
-import { GlobalStyle, iconSettings } from 'styles';
+import { GlobalStyle } from 'styles';
+import { ApplicationProvider } from 'Context/ApplicationContext';
 
-function App() {
+function Application() {
   return (
-    <VideosProvider>
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick={true}
-        rtl={false}
-        pauseOnFocusLoss={false}
-        draggable
-        pauseOnHover />
+    <ApplicationProvider>
       <GlobalStyle />
-      <IconContext.Provider value={iconSettings}>
-        <AuthProvider>
-          <Routes />
-        </AuthProvider>
-      </IconContext.Provider>
-    </VideosProvider>
+      <Routes />
+    </ApplicationProvider>
   );
 }
 
-export default App;
+export default Application;
