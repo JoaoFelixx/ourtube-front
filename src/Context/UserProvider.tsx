93207670f0b2bc411a135d1a1d103608466d1520
Felix developer <felixxdeveloper@gmail.com>
2022-08-11 19:57:33 -0300

    ADD - Created Context to all user data

---
+++ src/Context/UserProvider.tsx
@@ -0,0 +1,56 @@
+import React, {
+  useState,
+  useEffect,
+  useContext,
+  useCallback,
+  createContext,
+} from 'react';
+import { useSelectorAuth } from './AuthProvider';
+import { Provider, Enrolled } from 'interfaces';
+import { api } from 'service';
+
+interface User {
+  enrolled: Enrolled[];
+  dispatch?: React.Dispatch<Enrolled>;
+}
+
+const Context = createContext<User>({ enrolled: [] });
+
+const useSelectorUser = (): User => useContext(Context);
+
+function UserProvider({ children }: Provider) {
+  const [enrolled, setEnrolled] = useState<Enrolled[]>([]);
+  const { authenticated } = useSelectorAuth();
+
+  const dispatch = useCallback((subscribe: Enrolled) =>
+    setEnrolled([...enrolled, subscribe]), [enrolled]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const token = localStorage.getItem('ourtube_token')
+
+        if (!authenticated || !token)
+          return;
+
+        const headers = {
+          Authorization: `Bearer ${JSON.parse(token)}`
+        }
+
+        const { data } = await api.get<Enrolled[]>('user/subscribes', { headers });
+
+        setEnrolled(data);
+      } catch (error) {
+        return
+      }
+    })()
+  }, [authenticated])
+
+  return (
+    <Context.Provider value={{ enrolled, dispatch }}>
+      {children}
+    </Context.Provider>
+  )
+}
+
+export { useSelectorUser, UserProvider };
