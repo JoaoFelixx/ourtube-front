9391ec9f10e5608c9b70fb1aa7f8a05d9413f619
Felix developer <felixxdeveloper@gmail.com>
2022-08-18 19:55:17 -0300

    NEW - Created subscribe button

--- src/components/Subscribe/index.tsx
+++ src/components/Subscribe/index.tsx
@@ -3,10 +3,35 @@ import { api } from 'service';
 import { toast } from 'react-toastify';
 import { Enrolled, ID } from 'interfaces';
 import { useSelectorUser } from 'Context/UserProvider';
-import { SubscribeAtChannel, Subscribed } from './style';
+import { SubscribeButton } from './style';
 
 export function Subscribe({ id }: ID) {
   const { enrolled, dispatch } = useSelectorUser();
+
+  const unsubscribe = async () => {
+    try {
+      const token = localStorage.getItem('ourtube_token')
+
+      if (!token)
+        return
+
+      const headers = {
+        Authorization: `Bearer ${JSON.parse(token)}`
+      }
+
+      await api.delete(`/subscribe/${id}`, { headers });
+
+      const deletedEnrolled = enrolled.find(({ _id }) => _id === id);
+
+      if (!deletedEnrolled)
+        return
+
+      dispatch?.({ type: 'delete', enrolledSended: deletedEnrolled });
+
+    } catch (error) {
+      toast.error('Erro ao se desinscrever, tente novamente');
+    }
+  }
 
   const subscribe = async () => {
     try {
@@ -22,8 +47,8 @@ export function Subscribe({ id }: ID) {
       const data = { channel_id: id };
 
       const result = await api.post<Enrolled>('/subscribe', data, { headers });
-      
-      dispatch?.(result.data);
+
+      dispatch?.({ type: 'post', enrolledSended: result.data });
 
     } catch (error) {
       toast.error('Erro ao se inscrever, tente novamente');
@@ -33,8 +58,8 @@ export function Subscribe({ id }: ID) {
   return (
     <React.Fragment>
       {enrolled.find(({ channel_id }) => channel_id === id) ?
-        <Subscribed>INSCRITO</Subscribed> :
-        <SubscribeAtChannel onClick={subscribe}>INSCREVA-SE</SubscribeAtChannel>
+        <SubscribeButton isSubscribed onClick={unsubscribe}>INSCRITO</SubscribeButton> :
+        <SubscribeButton onClick={subscribe}>INSCREVA-SE</SubscribeButton>
       }
     </React.Fragment>
   )
--- src/components/Subscribe/style.tsx
+++ src/components/Subscribe/style.tsx
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
 
-const SubscribeAtChannel = styled.button` 
-	background-color: #ff0000; 
+interface IsSubscribed {
+	isSubscribed?: boolean; 
+}
+
+const SubscribeButton = styled.button<IsSubscribed>` 
+	background-color: ${props => props.isSubscribed ? '#9B9B9B' : '#ff0000'}; 
 	padding: 6px 12px;
 	border: none;
 	font-size: 1.2em;
@@ -13,16 +17,4 @@ const SubscribeAtChannel = styled.button`
 	}
 `;
 
-const Subscribed = styled.button`
-	background-color: #9B9B9B; 
-	padding: 6px 12px;
-	border: none;
-	font-size: 1.2em;
-	color: #fff;
-	
-	@media (max-width: 768px) {
-		margin-left: 2px;
-	}
-`;
-
-export { SubscribeAtChannel, Subscribed };
+export { SubscribeButton };
