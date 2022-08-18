49cb3ed356fd3dea12f301f67a6bde4f654f6962
Felix developer <felixxdeveloper@gmail.com>
2022-08-18 19:56:26 -0300

    ADD/REF - FormLogin is FormRegister ia in same component

--- src/components/forms/FormLogin/index.tsx
+++ src/components/forms/FormLogin/index.tsx
@@ -1,16 +1,21 @@
 import React, { useState } from 'react';
+import Ourtube from 'assets/icon.png';
 import { api } from 'service';
 import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
 import { useSelectorAuth } from 'Context/AuthProvider';
 import { Card, Input, Button } from './style';
-import Ourtube from 'assets/icon.png';
 
-export function FormLogin () {
+interface FormType {
+	isLogin?: boolean;
+}
+
+export function FormLogin({ isLogin }: FormType) {
+	const navigate = useNavigate();
 	const [email, setEmail] = useState<string | null>(null);
 	const [password, setPassword] = useState<string | null>(null);
+	const [confPassword, setConfPassword] = useState<string | null>(null);
 	const { setAuthenticated } = useSelectorAuth();
-	const navigate = useNavigate();
 
 	const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
 		try {
@@ -18,15 +23,31 @@ export function FormLogin () {
 
 			const user = { email, password };
 
-			const { data: { token } } = await api.post('/auth', user);
+			if (isLogin) {
+				const { data: { token } } = await api.post('/auth', user);
 
-			localStorage.setItem('ourtube_token', JSON.stringify(token));
+				localStorage.setItem('ourtube_token', JSON.stringify(token));
 
-			setAuthenticated?.(true);
+				setAuthenticated?.(true);
 
-			navigate('/');
+				navigate('/');
+
+				return
+			}
+
+			if (password !== confPassword) {
+				toast.warning('Senhas não são iguais');
+				return
+			}
+
+			await api.post('/users', user);
+
+			toast.success('Cadastrado com sucesso');
+
+			setTimeout(() => navigate('/login'), 2000);
+
 		} catch (error) {
-			toast.error('Erro')
+			toast.error('Erro ao efetuar processo');
 		}
 	}
 
@@ -34,21 +55,32 @@ export function FormLogin () {
 		<Card>
 			<form onSubmit={onSubmit} >
 				<div className="top">
-					<img src={Ourtube} alt="Icon ourtube"/> 
-					<h1 style={{ marginLeft: '4px' }}>Fazer Login</h1>
-				</div><br/>
+					<img src={Ourtube} alt="Icon ourtube" />
+					<h1 style={{ marginLeft: '4px' }}>{isLogin ? 'Fazer Login' : 'Registre-se'}</h1>
+				</div><br />
 				<label>Email</label>
-				<Input 
-					type="email" 
+				<Input
+					type="email"
 					required
 					onChange={(event) => setEmail(event.target.value)}
-					placeholder="username@user.com" /><br/><br/>
+					placeholder="username@user.com" /><br /><br />
 				<label>Senha</label>
-				<Input 
+				<Input
 					type="password"
 					required
-					onChange={(event) => setPassword(event.target.value)} 
-					placeholder="*******"/><br /><br/>
+					onChange={(event) => setPassword(event.target.value)}
+					placeholder="*******" /><br /><br />
+
+				{!isLogin && (
+					<React.Fragment>
+						<label>Confirmação de senha</label>
+						<Input
+							type="password"
+							required
+							onChange={(event) => setConfPassword(event.target.value)}
+							placeholder="*******" /><br /><br />
+					</React.Fragment>
+				)}
 				<div className="buttons">
 					<Button color='#019AFA'>Enviar</Button>
 					<Button type="reset" color='#ff0000'>Cancelar</Button>
