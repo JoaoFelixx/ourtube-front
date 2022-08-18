749cbd30fe6ac0ac55d6c65329bc3d2479ebc70b
Felix developer <felixxdeveloper@gmail.com>
2022-08-18 19:57:08 -0300

    REF - Verifying if is login or register page

--- src/containers/Login/index.tsx
+++ src/containers/Login/index.tsx
@@ -1,7 +1,11 @@
 import { FormLogin } from 'components';
+import { useLocation } from 'react-router-dom';
 
 export function Login() {
+	const { pathname } = useLocation();
+	const isLogin = pathname === '/login';
+
 	return (
-		<FormLogin />
+		<FormLogin isLogin={isLogin} />
 	)
 }
