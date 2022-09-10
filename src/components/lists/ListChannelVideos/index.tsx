fc7194f27b11cfca90ca67dd41e6464832b9c9bc
Felix developer <felixxdeveloper@gmail.com>
2022-09-10 13:01:03 -0300

    REF - Removing unusable interface and added message if not has videos

--- src/components/lists/ListChannelVideos/index.tsx
+++ src/components/lists/ListChannelVideos/index.tsx
@@ -1,39 +1,39 @@
 import React, { useEffect, useState, memo } from 'react';
-import { Video } from 'interfaces';
+import { Video, ID } from 'interfaces';
 import { useSelectorApp } from 'Context/ApplicationProvider';
 import { Item, List, Flex, Content, Preview } from './style';
 
-interface ListProps {
-	id?: string;
-}
-
-export function ListChannelVideos({ id }: ListProps) {
-	const [videosSelected, setVideosSelected] = useState<null | Video[]>(null);
+export function ListChannelVideos({ id }: ID) {
 	const { videos } = useSelectorApp();
+	const [videosSelected, setVideosSelected] = useState<Video[]>([]);
 
 	const Videos = memo(() => {
 		return (
-			<List>
-				{React.Children.toArray(
-					videosSelected?.map(({ _id, description, preview_src, channel_id }) => {
-						return (
-							<Item>
-								<a
-									style={{ textDecoration: 'none', color: '#000' }}
-									href={`/video/${_id}`} >
-									<Flex>
-										<Preview src={preview_src} alt={description} />
-										<Content>
-											<h3>{description}</h3>
-											<p>{channel_id.name}</p>
-										</Content>
-									</Flex>
-								</a>
-							</Item>
-						)
-					})
+			<React.Fragment>
+				{!id ? <p>Nenhum Video publicado</p> : (
+					<List>
+						{React.Children.toArray(
+							videosSelected?.map(({ _id, description, preview_src, channel_id }) => {
+								return (
+									<Item>
+										<a
+											style={{ textDecoration: 'none', color: '#000' }}
+											href={`/video/${_id}`} >
+											<Flex>
+												<Preview src={preview_src} alt={description} />
+												<Content>
+													<h3>{description}</h3>
+													<p>{channel_id.name}</p>
+												</Content>
+											</Flex>
+										</a>
+									</Item>
+								)
+							})
+						)}
+					</List>
 				)}
-			</List>
+			</React.Fragment>
 		)
 	})
 
@@ -44,7 +44,6 @@ export function ListChannelVideos({ id }: ListProps) {
 			return
 
 		setVideosSelected(videosWithSameId);
-
 	}, [id, videos]);
 
 	return <Videos />
