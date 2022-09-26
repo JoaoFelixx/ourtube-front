0e0413ab5fa21db14b52d3506adc369ebd2ff154
Felix developer <felixxdeveloper@gmail.com>
2022-09-26 17:55:17 -0300

    REF - Removed unused state, unused functions and better the performance

--- src/Context/ChannelProvider.tsx
+++ src/Context/ChannelProvider.tsx
@@ -12,7 +12,7 @@ import { localizedStrings } from 'constants/localizedStrings';
 import { Provider, ChannelAndEnrolled } from 'interfaces';
 
 interface DispatchOptions {
-  type: 'change:channel' | 'delete:channel' | 'toggle:modal';
+  type: 'change:channel';
   channel?: ChannelAndEnrolled;
 }
 
@@ -20,33 +20,45 @@ interface PanelProps {
   id?: string;
   channel?: ChannelAndEnrolled;
   location: string;
-  showModal: boolean;
   dispatch?: React.Dispatch<DispatchOptions>;
 }
 
-const Context = createContext<PanelProps>({
-  location: '',
-  showModal: false,
-});
+const Context = createContext<PanelProps>({ location: '' });
 
 const useSelectorChannel = (): PanelProps => useContext(Context);
 
 function ChannelProvider({ children }: Provider) {
   const { id } = useParams();
-  const location = window.location.pathname;
   const [channel, setChannel] = useState<PanelProps['channel']>(undefined);
-  const [showModal, setShowModal] = useState<boolean>(false);
+  const [location, setLocation] = useState<string>('');
 
-  const dispatch = useCallback(({ type, channel }: DispatchOptions) => ({
-    'change:channel': setChannel(channel),
-    'delete:channel': setChannel(undefined),
-    'toggle:modal': setShowModal(!showModal),
-  }[type]), [showModal]);
+  const dispatch = useCallback(({ type, channel: modifiedChannel }: DispatchOptions) => ({
+    'change:channel': setChannel(modifiedChannel),
+  }[type]), []);
 
   useEffect(() => {
-    (async () => {
-      try {
-        if (location !== '/myChannel') {
+    try {
+      (() => ({
+        '/myChannel': (async (): Promise<void> => {
+          const token = localStorage.getItem('ourtube_token');
+
+          if (!token)
+            return
+
+          const headers = {
+            Authorization: `Bearer ${JSON.parse(token)}`
+          }
+
+          const { data, status } = await api.get<ChannelAndEnrolled>('/myChannel', { headers });
+
+          if (status === 204)
+            return
+
+          setChannel(data);
+
+          return
+        })(),
+        '/channel': (async (): Promise<void> => {
           const { status, data } = await api.get<ChannelAndEnrolled>(`/channel/${id}`);
 
           if (status === 204)
@@ -55,32 +67,17 @@ function ChannelProvider({ children }: Provider) {
           setChannel(data);
 
           return
-        }
-
-        const token = localStorage.getItem('ourtube_token');
-
-        if (!token)
-          return
-
-        const headers = {
-          Authorization: `Bearer ${JSON.parse(token)}`
-        }
-
-        const { data, status } = await api.get<ChannelAndEnrolled>('/myChannel', { headers });
-
-        if (status === 204)
-          return
-
-        setChannel(data);
-
-      } catch (error) {
-        toast.error(localizedStrings.errorGettingData);
-      }
-    })()
+        })()
+      }[location] || setChannel(undefined)))()
+    } catch (error) {
+      toast.error(localizedStrings.errorGettingData)
+    }
   }, [id, location]);
 
+  useEffect(() => setLocation(window.location.pathname), []);
+
   return (
-    <Context.Provider value={{ id, channel, location, showModal, dispatch }}>
+    <Context.Provider value={{ id, channel, location, dispatch }}>
       {children}
     </Context.Provider>
   )
